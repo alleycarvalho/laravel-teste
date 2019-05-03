0456f60a2a8ad980efdcfc7b65c954ebd2f11158
Alley M. Carvalho <alleycarvalho@gmail.com>
2019-05-03 00:58:31 -0300

    Alteração de exemplo após integração com o GCP

--- app/resources/views/welcome.blade.php
+++ app/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    ViewSet
+                    ViewSet Teste
                 </div>
 
                 <div class="links">
