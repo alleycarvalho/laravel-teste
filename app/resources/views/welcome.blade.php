955d2004cc042df63fa9ce9f83cca94553ef635a
Alley M. Carvalho <alleycarvalho@gmail.com>
2019-05-03 00:19:58 -0300

    Alteração de exemplo

--- app/resources/views/welcome.blade.php
+++ app/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    ViewSet
                 </div>
 
                 <div class="links">
